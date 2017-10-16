3522fb20326797b39c69b31a8456ccd24f3eff8f
niawkung <niawkung2@gmail.com>
2017-10-16 18:29:34 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import list from './list';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {
+          list.map(item =>
+             (
+            <div key={ item.objectID }>
+            <h1> <a href={ item.url }> { item.title }</a> by {item.author}</h1>
+                  <h4> { item.num_comments } Comments | { item.points } Points </h4>
+            </div>
+            )
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/list.js
@@ -0,0 +1,20 @@
+const list = [
+  {
+    title: 'Tell me the truth without words',
+    url: 'http//google.com',
+    author: 'dd',
+    num_comments: 100,
+    points: 50,
+    objectID: 1
+  },
+  {
+    title: 'Tell me the truth without words',
+    url: 'http//facebook.com.com',
+    author: 'ninja',
+    num_comments: 100,
+    points: 50,
+    objectID: 1
+  }
+]
+
+export default list;
